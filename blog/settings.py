0a59b56ff56d21dd023954825a34bf746cdcd936
minakim <b07705043@ntu.edu.tw>
2022-04-29 19:35:25 +0800

    followed blog

--- blog/settings.py
+++ blog/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dvuwu=x!)o_w9+^0inz^(4009d*+b(i#hv#cyohzj4l#ns^xi3'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['minadjangoapp.herokuapp.com']
 
 
 # Application definition
@@ -134,3 +135,8 @@ AUTH_USER_MODEL = 'authentication.CustomUser' # authentication is the app name
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+import django_heroku
+django_heroku.setting(
+    locals()
+)
